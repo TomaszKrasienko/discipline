ea6f968995ed70fdbe0e6c7d8f8c111a61591a8c
Tomasz Krasienko <tomasz.krasienko@outlook.com>
2025-05-29 21:29:40 +0200

    Added facade and endpoint for deleting activity rule stage

--- discipline.wasm.ui/src/discipline.ui.communication.http/ActivityRules/IActivityRulesHttpService.cs
+++ discipline.wasm.ui/src/discipline.ui.communication.http/ActivityRules/IActivityRulesHttpService.cs
@@ -1,5 +1,4 @@
 using discipline.ui.communication.http.ActivityRules.DTOs.Requests;
-using discipline.ui.communication.http.DailyTrackers.Requests;
 using Refit;
 
 namespace discipline.ui.communication.http.ActivityRules;
@@ -14,6 +13,9 @@ public interface IActivityRulesHttpService
     
     [Delete("/api/activity-rules/{id}")]
     public Task<HttpResponseMessage> DeleteActivityRule(string id, CancellationToken cancellationToken);
+    
+    [Delete("/api/activity-rules/{activityRuleId}/stages/{stagesId}")]
+    public Task<HttpResponseMessage> DeleteActivityRuleStage(string activityRuleId, string stagesId, CancellationToken cancellationToken);
     
     [Post("/api/activity-rules")]
     public Task<HttpResponseMessage> CreateActivityRule(CreateActivityRuleRequestDto request, CancellationToken cancellationToken);
