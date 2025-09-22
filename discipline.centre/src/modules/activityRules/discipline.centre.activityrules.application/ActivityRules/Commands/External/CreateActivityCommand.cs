26256b6b27bbc3d07f5a6138ad0d1a7fa8080dd1
Tomasz Krasieńko <94125393+TomaszKrasienko@users.noreply.github.com>
2025-09-22 22:04:10 +0200

    Fixed command

--- discipline.centre/src/modules/activityRules/discipline.centre.activityrules.application/ActivityRules/Commands/External/CreateActivityCommand.cs
+++ discipline.centre/src/modules/activityRules/discipline.centre.activityrules.application/ActivityRules/Commands/External/CreateActivityCommand.cs
@@ -1,5 +1,4 @@
 using discipline.centre.activityrules.application.ActivityRules.DTOs.Commands;
-using discipline.centre.shared.abstractions.SharedKernel.TypeIdentifiers;
 using discipline.libs.cqrs.abstractions.Commands;
 
 namespace discipline.centre.activityrules.application.ActivityRules.Commands.External;
